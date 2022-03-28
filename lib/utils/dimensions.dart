fb5d4da3d6139cd099e2e6d57d4132464dd154ba
0zyyy <jalufazlurrahman@gmail.com>
2022-03-28 15:28:17 +0700

    class dimensions supaya bisa lebih dinamis ukuran

--- lib/utils/dimensions.dart
+++ lib/utils/dimensions.dart
@@ -1,3 +1,9 @@
+import 'package:get/get.dart';
+
 class Dimensions {
+  static double screenWidth = Get.context!.width;
+  static double screenHeight = Get.context!.height;
   static const double padding = 16.0;
+
+  static double popularImgSize = screenHeight / 2.41;
 }
